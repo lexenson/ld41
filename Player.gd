b610f2a11e1c0040a6460fa9189f13884cac0654
Finn Pauls <derfinn@gmail.com>
2018-04-21 19:53:23 +0200

    Player can jump

--- Player.gd
+++ Player.gd
@@ -1,23 +1,31 @@
 extends Area2D
 
 export (int) var speed = 300
+export (int) var jump_speed = 1000
+
 var velocity = Vector2()
 
 func control(delta):
-	velocity = Vector2()
+	velocity.x = 0
 	if Input.is_action_pressed('ui_left'):
 		$AnimatedSprite.animation = 'walk'
-		velocity = Vector2(-1,0) * speed
+		velocity.x = -1 * speed
 		$AnimatedSprite.flip_h = true
 	elif Input.is_action_pressed('ui_right'):
 		$AnimatedSprite.animation = 'walk'
-		velocity = Vector2(1,0) * speed
-		$AnimatedSprite.flip_h = false		
-	else:
-		$AnimatedSprite.animation = 'idle'
+		velocity.x = 1 * speed
+		$AnimatedSprite.flip_h = false
+		
+	if Input.is_action_pressed('ui_up') and position.y == 800 - 64:
+		velocity.y = -1 * jump_speed
 		
 	position += velocity * delta
-	position = Vector2(max(32, min(1200 - 32, position.x)), position.y)
+	velocity.y += gravity
+	
+	if (velocity.x == 0 and velocity.y == 0):
+		$AnimatedSprite.animation = 'idle'
+		
+	position = Vector2(max(32, min(1200 - 32, position.x)), min(800 - 64, position.y))
 	
 func _process(delta):
 	control(delta)
