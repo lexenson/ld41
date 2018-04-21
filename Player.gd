2a2290e8a34c8bb2832868cfec283d12669c596c
Finn Pauls <derfinn@gmail.com>
2018-04-21 20:15:28 +0200

    Fix idle player

--- Player.gd
+++ Player.gd
@@ -7,6 +7,8 @@ var velocity = Vector2()
 
 func control(delta):
 	velocity.x = 0
+	var ground_touched = position.y == 800 - 64
+
 	if Input.is_action_pressed('ui_left'):
 		$AnimatedSprite.animation = 'walk'
 		velocity.x = -1 * speed
@@ -16,13 +18,13 @@ func control(delta):
 		velocity.x = 1 * speed
 		$AnimatedSprite.flip_h = false
 		
-	if Input.is_action_pressed('ui_up') and position.y == 800 - 64:
+	if Input.is_action_pressed('ui_up') and ground_touched:
 		velocity.y = -1 * jump_speed
 		
 	position += velocity * delta
 	velocity.y += gravity
 	
-	if (velocity.x == 0 and velocity.y == 0):
+	if (velocity.x == 0 and ground_touched):
 		$AnimatedSprite.animation = 'idle'
 		
 	position = Vector2(max(32, min(1200 - 32, position.x)), min(800 - 64, position.y))
