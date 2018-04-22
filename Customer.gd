420aa6dc3dab81fb2c1be50721469f2aa94ec03b
Alexander Assmann <alexander.assmann91@gmail.com>
2018-04-22 15:47:22 +0200

    Add paying state to customer

--- Customer.gd
+++ Customer.gd
@@ -11,29 +11,47 @@ func _ready():
 	$AnimatedSprite.play()
 
 func _process(delta):
-	if consuming:
-		$BeerThoughtBubble.visible = false
-		if consuming.empty:
-			state = "LEAVING"
-			emit_signal("paid")
-			consuming = null
-	elif state == "LEAVING":
-		$BeerThoughtBubble.visible = false
-	else:
-		$BeerThoughtBubble.visible = true
-
 	if state == "COMING":
+		$ThoughtBubble.visible = false		
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_h = false
 		position.x += speed
 		if (position.x >= get_node(assigned_seat_position).position.x):
 			state = "SITTING"
+		$CollisionShape2D.disabled = true
 	elif state == "LEAVING":
+		$ThoughtBubble.visible = false		
 		$AnimatedSprite.animation = "walk"
 		$AnimatedSprite.flip_h = true
 		position.x -= speed
+		$CollisionShape2D.disabled = true		
 	elif state == "SITTING":
+		show_beer_bubble()
 		$AnimatedSprite.animation = "sit"
+		$CollisionShape2D.disabled = false
+		if consuming:
+			state = "DRINKING"
+	elif state == "DRINKING":
+		$ThoughtBubble.visible = false
+		$AnimatedSprite.animation = "sit"
+		$CollisionShape2D.disabled = true
+		if consuming and consuming.empty:
+			state = "PAYING"
+			consuming = null
+	elif state == "PAYING":
+		$CollisionShape2D.disabled = true
+		show_coin_bubble()
+		$AnimatedSprite.animation = "sit"
+		
+func show_coin_bubble():
+	$ThoughtBubble.visible = true
+	$ThoughtBubble/Coin.visible = true
+	$ThoughtBubble/Beer.visible = false
+	
+func show_beer_bubble():
+	$ThoughtBubble.visible = true
+	$ThoughtBubble/Coin.visible = false
+	$ThoughtBubble/Beer.visible = true
 
 
 func _on_Beer_taken_by(taken, taker):
