44e6e0a51d9bb44ec3a97a1fe261279726988d57
Alexander Assmann <alexander.assmann91@gmail.com>
2018-04-23 20:39:50 +0200

    Make objects swapable

--- Food.gd
+++ Food.gd
@@ -3,14 +3,25 @@ extends Area2D
 export (bool) var flip = false
 var empty = false
 signal taken_by(taken, taker)
+var can_be_taken = true
+
+
+func _ready():
+	$NoSwapTimer.connect("timeout", self, "set_can_be_taken")
 
 func _process(delta):
 	$AnimatedSprite.flip_h = flip
 	
+func set_can_be_taken():
+	can_be_taken = true	
 
 func _on_Food_area_entered(area):
 	var parent = area.get_parent()
-	if area.name == "Player" and not area.holding:
+	if area.name == "Player" and can_be_taken and (not area.holding or area.holding.can_be_taken):
+		if area.holding:
+			area.holding.position = self.position
+		can_be_taken = false
+		$NoSwapTimer.start()
 		emit_signal("taken_by", self, area)
 	if parent.name == "Customers" and not empty and area.get('state') == 'SITTING':
 		emit_signal("taken_by", self, area)
